ab639eb7d52180ac0862791213fafbd5e8e5fd34
Chris <creedrogers@gmail.com>
2026-01-02 18:01:07 -0500

    added some sprite stuff

--- src/entities.hpp
+++ src/entities.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
+#include "animated-sprite.hpp"
+
 #include <raylib.h>
 
 struct Transform   { Vector2 position; float rotation; };
 struct Velocity    { Vector2 direction; float speed; };
 struct PlayerInput { bool thrust; bool shoot, rotateLeft, rotateRight; };
 
-struct Sprite { Texture2D }
-
-struct Player { Transform transform; Velocity velocity; int lives; float shootCooldown; };
-struct Asteroid { Transform transform; Velocity velocity; float size;
-
+struct Player   { Transform transform; Velocity velocity; int lives; float shootCooldown; };
+struct Asteroid { Transform transform; Velocity velocity; float size; AnimatedSprite sprite; };
+struct Bullet   { Transform transform; };
