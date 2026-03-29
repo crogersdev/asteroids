58f42d15c8182b3d198ba478680bb60cfd7a78dc
Chris <creedrogers@gmail.com>
2026-03-29 15:39:12 -0400

    ship moves but not super great

--- src/systems.hpp
+++ src/systems.hpp
@@ -2,6 +2,7 @@
 
 #include "entities.hpp"
 
+#include <iostream>
 #include <random>
 
 #include <raylib.h>
@@ -9,7 +10,7 @@
 namespace crogersdev {
 
 inline void draw_debug_info() {
-    DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 2.0, WHITE);
+    DrawCircle(GetScreenWidth() / 2.f, GetScreenHeight() / 2.f, 2.f, WHITE);
 }
 
 inline void game_init(Registry& registry) {
@@ -17,17 +18,21 @@ inline void game_init(Registry& registry) {
     float y = GetScreenHeight();
     Vector2 center = { x / 2.f, y / 2.f };
 
+    float player_thrust_speed = 125.f;
+    float player_turn_speed = .05f;
+    float player_shoot_rate = 1.5f;
+
     Entity player = registry.create();
     registry.add(player, PlayerInput{ false, false, false, false });
     registry.add(player, TimesFired{ 0 });
-    registry.add(player, Transform{ { center.x, center.y }, PI / 120.f });
-    registry.add(player, Velocity{ { 0.f, 0.f }, 0.f });
-    registry.add(player, WeaponCooldown{ 1.5f });
+    registry.add(player, Transform{ { center.x, center.y }, player_turn_speed });
+    registry.add(player, Velocity{ { 0.f, -1.f }, 125.f });
+    registry.add(player, WeaponCooldown{ player_shoot_rate });
     registry.add(player, PolygonShip{{
-        Line{{ center.x-10.f, center.y+4.f },  { center.x,      center.y-14.f }, RED },
-        Line{{ center.x,      center.y-14.f }, { center.x+10.f, center.y+4.f },  BLUE },
-        Line{{ center.x+10.f, center.y+4.f },  { center.x,      center.y },      GREEN },
-        Line{{ center.x,      center.y },      { center.x-10.f, center.y+4.f },  YELLOW },
+        Line{{ -10.f,  +4.f }, {   0.f, -14.f }, RED },
+        Line{{   0.f, -14.f }, { +10.f,  +4.f }, BLUE },
+        Line{{ +10.f,  +4.f }, {   0.f,   0.f }, GREEN },
+        Line{{   0.f,   0.f }, { -10.f,  +4.f }, YELLOW },
     }});
 
     constexpr int initialAsteroids = 5;
@@ -49,7 +54,7 @@ inline void game_init(Registry& registry) {
         if (a_y > one_third.y) a_y += one_third.y;
 
         asteroids[i] = registry.create();
-        registry.add(asteroids[i], Transform{ { a_x, a_y }, 0.0 });
+        registry.add(asteroids[i], Transform{ { a_x, a_y }, 0.f });
         registry.add(asteroids[i], Size{ 4 });
         registry.add(asteroids[i], Asteroid{{
             Line{{ -40.f, +40.f }, { -40.f, -40.f }, RED },
@@ -69,20 +74,11 @@ inline void player_input_system(Registry& registry) {
     for (Entity& player_id : registry.view<PlayerInput>()) {
         auto& player = registry.get<PlayerInput>(player_id);
 
-        if (IsKeyDown(KEY_W)) {
-            player.thrust = true;
-        }
-        if (IsKeyDown(KEY_A)) {
-            player.rotate_left = true;
-        }
-        if (IsKeyDown(KEY_S)) {
-        }
-        if (IsKeyDown(KEY_D)) {
-            player.rotate_right = true;
-        }
-        if (IsKeyDown(KEY_SPACE)) {
-            player.shoot = true;
-        }
+        if (IsKeyDown(KEY_W))     { player.thrust = true; }
+        if (IsKeyDown(KEY_A))     { player.rotate_left = true; }
+        if (IsKeyDown(KEY_S))     { }
+        if (IsKeyDown(KEY_D))     { player.rotate_right = true; }
+        if (IsKeyDown(KEY_SPACE)) { player.shoot = true; }
     }
 }
 
@@ -100,23 +96,24 @@ inline void fire_frequency_system(Registry& registry) {
     for (Entity& shooter : registry.view<TimesFired>()) {
         // todo: you're going to have to add a clock system
         //       and component and when that's done you can
-        //       like, idk, allow fire frequency to reset.
+        //       like, idk, allow fire frequency to reset.uo9 ikn,m 
     }
 }
 
 inline void movement_update_system(Registry& registry) {
+    auto w = GetScreenWidth();
+    auto h = GetScreenHeight();
+
     for (Entity& e : registry.view<Asteroid, Transform, Velocity>()) {
-        auto& t = registry.get<Transform>(e);
-        auto& v = registry.get<Velocity>(e);
+        auto& transform = registry.get<Transform>(e);
+        auto& velocity = registry.get<Velocity>(e);
 
-        Vector2 dt_offset = { v.direction.x * v.speed * GetFrameTime(),
-                              v.direction.y * v.speed * GetFrameTime() };
+        Vector2 dt_offset = { velocity.direction.x * velocity.speed * GetFrameTime(),
+                              velocity.direction.y * velocity.speed * GetFrameTime() };
 
-        t.position.x += dt_offset.x;
-        t.position.y += dt_offset.y;
+        transform.position.x += dt_offset.x;
+        transform.position.y += dt_offset.y;
 
-        auto w = GetScreenWidth();
-        auto h = GetScreenHeight();
         // explain: if this is confusing, it's because we're using fmod to 
         //          screen wrap but in a way that respects negative numbers
         //          e.g.
@@ -127,53 +124,51 @@ inline void movement_update_system(Registry& registry) {
         //          ^^^ if asteroid goes off left edge by 10.  rework with
         //          positive 10 and you'll see you correctly get 10 with the same
         //          code
-        t.position.x = fmod(fmod(t.position.x, w) + w, w);
-        t.position.y = fmod(fmod(t.position.y, h) + h, h);
+        transform.position.x = fmod(fmod(transform.position.x, w) + w, w);
+        transform.position.y = fmod(fmod(transform.position.y, h) + h, h);
     }
 
     for (Entity& e : registry.view<Transform, Velocity, PlayerInput, PolygonShip>()) {
-        auto& t = registry.get<Transform>(e);
-        auto& v = registry.get<Velocity>(e);
-        auto& p = registry.get<PlayerInput>(e);
-        auto& player = registry.get<PolygonShip>(e);
+        auto& transform = registry.get<Transform>(e);
+        auto& velocity = registry.get<Velocity>(e);
+        auto& player = registry.get<PlayerInput>(e);
+        auto& ship = registry.get<PolygonShip>(e);
 
-        auto center = t.position;
+        if (player.rotate_left || player.rotate_right) {
+            Vector2 new_start = {}, new_end = {};
+            float t = transform.rotation_speed;
+            if (player.rotate_left) { t *= -1.f; }
 
-        for(auto& ship_edge : player.lines) {
-            Vector2 old_start = {
-                ship_edge.start.x - center.x,
-                ship_edge.start.y - center.y };
+            for (auto& ship_edge : ship.lines) {
+                new_start.x = ship_edge.start.x * cos(t) - ship_edge.start.y * sin(t);
+                new_start.y = ship_edge.start.x * sin(t) + ship_edge.start.y * cos(t);
 
-            Vector2 old_end = {
-                ship_edge.end.x - center.x,
-                ship_edge.end.y - center.y };
-
-            if (p.rotate_left || p.rotate_right) {
-                auto theta = t.rotation_speed;
-                if (p.rotate_left) { theta *= -1.f; }
-                Vector2 new_start = {
-                    old_start.x * cos(theta) - old_start.y * sin(theta),
-                    old_start.x * sin(theta) + old_start.y * cos(theta) };
-
-                Vector2 new_end = {
-                    old_end.x * cos(theta) - old_end.y * sin(theta),
-                    old_end.x * sin(theta) + old_end.y * cos(theta) };
-
-                new_start.x += center.x;
-                new_start.y += center.y;
-                new_end.x += center.x;
-                new_end.y += center.y;
+                new_end.x = ship_edge.end.x * cos(t) - ship_edge.end.y * sin(t);
+                new_end.y = ship_edge.end.x * sin(t) + ship_edge.end.y * cos(t);
 
                 ship_edge.start = new_start;
-                ship_edge.end = new_end;
+                ship_edge.end   = new_end;
             }
 
-            if (p.thrust) {
+            Vector2 new_direction = {};
+            new_direction.x = velocity.direction.x * cos(t) - velocity.direction.y * sin(t);
+            new_direction.y = velocity.direction.x * sin(t) + velocity.direction.y * cos(t);
 
-            }
+            velocity.direction = new_direction;
+        }
 
-            if (p.shoot) {
-            }
+        if (player.thrust) {
+            Vector2 dt_offset = { velocity.direction.x * velocity.speed * GetFrameTime(),
+                                  velocity.direction.y * velocity.speed * GetFrameTime() };
+
+            transform.position.x += dt_offset.x;
+            transform.position.y += dt_offset.y;
+
+            transform.position.x = fmod(fmod(transform.position.x, w) + w, w);
+            transform.position.y = fmod(fmod(transform.position.y, h) + h, h);
+        }
+
+        if (player.shoot) {
         }
     }
 }
@@ -182,21 +177,32 @@ inline void render_system(Registry& registry) {
     // note: raylib's approach is to have global access to raylib managed
     //       resources.  in this case, that's the window to which we render
     //       and draw all our stuff.  that means we can just straight up
-    //       call DrawLineV without any problems or concerns.
+    //       call DrawLineEx without any problems or concerns
+    float line_thickness = 1.5f;
 
-    for (Entity& e : registry.view<PolygonShip>()) {
+    for (Entity& e : registry.view<PolygonShip, Transform>()) {
         auto& ship = registry.get<PolygonShip>(e);
+        auto& transform = registry.get<Transform>(e);
+
+        Vector2 start = {}, end = {};
         for (auto& ship_edge : ship.lines) {
-            DrawLineV(ship_edge.start, ship_edge.end, ship_edge.color);
+            start.x = ship_edge.start.x + transform.position.x;
+            start.y = ship_edge.start.y + transform.position.y;
+            end.x = ship_edge.end.x + transform.position.x;
+            end.y = ship_edge.end.y + transform.position.y;
+
+            DrawLineEx(start, end, line_thickness, ship_edge.color);
         }
     }
+
     for (Entity& e : registry.view<Asteroid, Transform>()) {
         auto& transform = registry.get<Transform>(e);
         auto& asteroid = registry.get<Asteroid>(e);
         for (auto& asteroidEdge : asteroid.lines) {
-            DrawLineV({ asteroidEdge.start.x + transform.position.x, asteroidEdge.start.y + transform.position.y },
-                      { asteroidEdge.end.x + transform.position.x, asteroidEdge.end.y + transform.position.y },
-                      asteroidEdge.color);
+            DrawLineEx({ asteroidEdge.start.x + transform.position.x, asteroidEdge.start.y + transform.position.y },
+                       { asteroidEdge.end.x + transform.position.x, asteroidEdge.end.y + transform.position.y },
+                       line_thickness,
+                       asteroidEdge.color);
         }
     }
 }
